a8a45714872df02cb41fc7506d86695c0552b35c
LebedevWeb <swansever@gmail.com>
2020-08-24 05:27:54 +0300

    The homework 1.2 test is perfectly completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,51 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = Math.pow(b,2) - (4 * a * c),
+        firstSquareRoot = (- b + Math.sqrt(d)) / (2 * a),
+        secondSquareRoot = (- b - Math.sqrt(d)) / (2 * a),
+        squareRoots = [];
+
+    if (d < 0) {
+        return squareRoots;
+    } else if (d === 0) {
+        squareRoots.push(firstSquareRoot);
+        return squareRoots;
+    } else if (d > 0) {
+        squareRoots.push(firstSquareRoot);
+        squareRoots.push(secondSquareRoot);
+        return squareRoots;
+    }
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let lotMarks = marks.length,
+        summMarks = 0,
+        averageMarks;
+
+    if (lotMarks === 0) {
+      return lotMarks;
+    } else {
+      if (lotMarks > 5) {
+        console.log(lotMarks);
+        marks = marks.slice(0, 5);
+        lotMarks = marks.length;
+      }
+      for (let i = 0; i < lotMarks; i++) {
+        summMarks = summMarks + marks[i];
+      }
+      averageMarks = summMarks / lotMarks;
+      return averageMarks;
+    }
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let dateOfBirth = dateOfBirthday.getFullYear(),
+        currentYear = new Date().getFullYear(),
+        age = currentYear - dateOfBirth;
+
+    if (age >= 18 ) {
+      return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+      return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
