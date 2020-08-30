cb9d66b4cdef2057d1ceff92223b6577709adb11
LebedevWeb <swansever@gmail.com>
2020-08-30 21:22:20 +0300

    The homework 1.3 test is perfectly completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,48 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	let today = new Date(),
+			s = Number(amount) - Number(contribution),
+			p = Number(percent) / 1200,
+			n = (date.getFullYear() - today.getFullYear()) * 12 + date.getMonth() - today.getMonth(),
+			paramName = '', paramData = '',
+			totalAmount = '';
+
+
+		if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNaN(date) && date > new Date() ) {
+			let payPerMonth = s*(p+p/(((1+p)**n)-1));
+			totalAmount = +(payPerMonth * n).toFixed(2);
+		} else if (isNaN(percent)) {
+			paramName = '"Процентная ставка"';
+			paramData = percent;
+			totalAmount = `Параметр ${paramName} содержит неправильное значение ${paramData}`
+		} else if (isNaN(contribution)) {
+			paramName = '"Начальный взнос"';
+			paramData = contribution;
+			totalAmount = `Параметр ${paramName} содержит неправильное значение ${paramData}`
+		} else if (isNaN(amount)) {
+			paramName = '"Общая стоимость"';
+			paramData = amount;
+			totalAmount = `Параметр ${paramName} содержит неправильное значение ${paramData}`
+		} else if (isNaN(date)) {
+			paramName = '"Общая стоимость"';
+			paramData = date;
+			totalAmount = `Параметр ${paramName} содержит неправильное значение ${paramData}`
+		} else if (date < new Date()) {
+			totalAmount = 'Дата не может быть меньше текущей';
+		}
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+
+    if (name === null || name === undefined || name.length === 0) {
+    	greeting = 'Привет, мир! Меня зовут Аноним';
+    } else if (name.length > 0) {
+    	greeting = `Привет, мир! Меня зовут ${name}`
+    }
+
+    return greeting;
 }
