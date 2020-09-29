c7908f56ae2e371cf25f389144623f8426e85265
LebedevWeb <swansever@gmail.com>
2020-09-30 01:07:23 +0300

    The homework 2.2 test is perfectly completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,36 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"
+// String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function () {
+  const str1 = this.replace(/[\s.,%+-]/g, '').toLowerCase();
+  const str2 = str1.split('').reverse().join('');
+  return str1 === str2;
 }
 
+//Решение задачи 2
+function getAverageMark(marks) {
+  let summ = 0, roundedAverage = 0;
+
+  if (marks.length > 0) {
+    for (let i = 0; i < marks.length; i++) {
+      summ += marks[i]
+    }
+    roundedAverage = Math.round(summ / marks.length);
+  }
+
+  return roundedAverage;
+}
+
+//Решение задачи 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = new Date(new Date().getFullYear(),new Date().getMonth(),new Date().getDate()),
+      birthDay = new Date(birthday);
+
+      birthday = birthDay.getTime();
+
+  let diff = now - birthday,
+      age = diff / (1000 * 60 * 60 * 24 * 365),
+      verdict = age > 18;
+
+  return verdict
 }
